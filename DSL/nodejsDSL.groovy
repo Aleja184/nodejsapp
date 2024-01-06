8e913e1103c0b36372e3129486cd7b740b4acfe8
Alejandra Toro <78225423+Aleja184@users.noreply.github.com>
2024-01-05 19:06:46 -0500

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Aleja184/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('Aleja184')
+            node / gitConfigEmail('torogrisalesa@gmail.com')
         }
     }
     triggers {
